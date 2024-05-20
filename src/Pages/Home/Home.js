819a69dc0b949b3e2c840065cddb45b1ee60b0f6
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-05-20 15:17:55 +0600

    introduced changes for home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,27 @@
-import './Home.css';
+import React from 'react';
+import Slider from '../Components/Slider';
+import '//Home.css';
+import chanelLogo from '../../img/cart-ic.png';
 
-function Home() {
+const Home = () => {
   return (
-    <>Home</>
+    <div className="home-container">
+      <header className="home-header">
+        <img src={chanelLogo} alt="Chanel Logo" className="home-logo" />
+        <h1 className="home-title">Добро пожаловать в мир CHANEL</h1>
+      </header>
+
+      <section className="home-slider">
+        <Slider />
+      </section>
+
+      
+
+      <footer className="home-footer">
+        <p>© {new Date().getFullYear()} CHANEL. Все права защищены.</p>
+      </footer>
+    </div>
   );
-}
+};
 
 export default Home;
